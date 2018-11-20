3b7b170c2799df5479e25bf625837c912bb9767b
hongfz16 <hongfz16@163.com>
2018-11-20 12:43:54 +0800

    finish build ftp-gui

--- client/gui/ftpFileModel.py
+++ client/gui/ftpFileModel.py
@@ -19,7 +19,7 @@ from PyQt5.QtGui import (QStandardItemModel, QIcon, QStandardItem,
 class LocalFileSystemModel(QStandardItemModel):
     def __init__(self):
         super().__init__()
-        self.path=Path('/Users/hongfz')
+        self.path=Path('/')
         self.setupItem()
 
     def setupItem(self):
